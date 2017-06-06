632ef098bf182bb10b124b53b17e39e38a66c2d2
BalajiStark <balajicse.2910@gmail.com>
2017-06-07 00:27:13 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class loop {
+
+    public static void main(String[] args) {
+     Scanner in=new Scanner(System.in);
+     int[] a=new int[args.length];
+     int n=args.length,sum=0;
+     for(int i=0;i<n;i++)
+     {
+         a[i]=Integer.parseInt(args[i]);
+         sum=sum+a[i];
+     }
+        System.out.println(sum);
+     
+    }
+    
+}
