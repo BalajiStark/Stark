52c1bbc35ae4fcadcebdde8855e54835cf4016db
BalajiStark <balajicse.2910@gmail.com>
2017-06-07 00:31:08 +0530

    Create countt.java

---
+++ countt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class countt {
+
+    public static void main(String[] args) {
+     Scanner in=new Scanner(System.in);
+     int n=in.nextInt();
+     String s=Integer.toString(n);
+        System.out.println(s.length());
+     
+    }
+    
+}
